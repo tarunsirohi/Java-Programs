0468d856cb5a7933c34ddc393eb440c20133b39f
Tarun Sirohi <tarunsirohi22@gmail.com>
2024-01-16 09:34:22 -0800

    Add File

---
+++ LeftNumPattern.java
@@ -0,0 +1,14 @@
+public class LeftNumPattern {
+    public static void main(String args[])
+    {
+        int i,j,row=4;
+        for(i=1;i<=row;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(" "+j);
+            }
+            System.out.println(" ");
+        }
+    }
+}
