917bede1fd562488e86176666996e54534a028f7
Tarun Sirohi <tarunsirohi22@gmail.com>
2024-01-17 07:20:05 -0800

    Add File

---
+++ LeftTriangleStar.java
@@ -0,0 +1,14 @@
+public class LeftTriangleStar {
+    public static void main(String args[])
+    {
+        int i,j,row=4;
+        for(i=0;i<row;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
