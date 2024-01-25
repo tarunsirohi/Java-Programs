ea6e46c36c817151e41b68b60e8caf25035e0461
Tarun Sirohi <tarunsirohi22@gmail.com>
2024-01-25 08:48:30 -0800

    Add File

---
+++ SumOfOdd.java
@@ -0,0 +1,11 @@
+public class SumOfOdd {
+    public static void main(String []args){
+        int i,sum=0;
+        for(i=10;i<=50;i++){
+            if(i%2!=0){
+                sum+=i;
+            }
+        }
+    System.out.println("Sum of odd numbers from 10 to 50 : "+sum);
+    }
+}
